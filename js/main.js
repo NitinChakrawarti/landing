3408450835f0005416c3cab49d860f92f7abf6bd
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2024-09-14 03:57:30 +0530

    done

--- js/main.js
+++ js/main.js
@@ -45,13 +45,44 @@ const sr  = ScrollReveal({
     origin:'bottom',
     distance:'60px',
     duration:3000,
-    delay:600
+    delay:100
 })
 
-
 sr.reveal('.hero__text', {origin:'top'})
+sr.reveal('.steps__step', {distance:'100px', interval:100})
+sr.reveal('.about__text ', {distance:'100px', interval:100})
+sr.reveal('.about__text', {origin:'left'})
+sr.reveal('.about__img', {origin:'right', delay:800})
+
+sr.reveal('.testiminial__bg', { delay:800})
+sr.reveal('.testiminial__title')
+sr.reveal('.testiminial__slider', { delay:1000})
+
+sr.reveal('.brand__img', { delay:600, distance:'100px' , interval:100})
+sr.reveal('.work__title')
+
+sr.reveal('.work__subtitle', {delay:800})
+
+sr.reveal('.work__grid', {delay:100})
+
+
+sr.reveal('.stats')
+
+
+sr.reveal('.stats__item ', {distance:'100px', interval:100})
+
+sr.reveal('.news__title ')
+
+sr.reveal('.news__subtitle ', {delay:800})
+
+sr.reveal('.news__item ', {distance:'100px', interval:100, delay:1000})
+
+
+sr.reveal('.contact__contanier')
+
+sr.reveal('.contact__text', {delay:800, })
+sr.reveal('.footer__item', {distance:'100px', interval:100, })
 
 
 
-
-
+sr.reveal('.footer_copyright')
