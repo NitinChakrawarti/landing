44215e91c042b6845ea964882bfa690f83e0b0f6
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2024-09-14 00:30:13 +0530

    hii

--- js/main.js
+++ js/main.js
@@ -7,3 +7,40 @@ navtig.addEventListener('click', ()=>{
     
 });
 
+
+// swiper 
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    slidesPerView: 3,
+    spaceBetween:20 ,
+    breakpoints: {
+        320:{
+            slidesPerView:1,
+        },
+        
+        960:{
+            slidesPerView:2,
+        },
+        1200:{
+            slidesPerView:3,
+        },
+
+    },
+});
+
+
+const bullets = document.querySelector('.swiper-pagination-bullet'); 
+
+bullets.style.backgroundColor = '#292f36';
+
+
+
+
+
+
